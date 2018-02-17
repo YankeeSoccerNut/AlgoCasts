08c97494d0299110c325b718b26a0e39aec2889d
Scott Anderson <yankeesoccernut@gmail.com>
2018-02-17 13:49:16 -0500

    key is test for last chunk size and splice()

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    let newArray = [];
+    let numChunks = (array.length % size === 0) ? array.length / size : Math.round(array.length / size);
+
+    for (let i = 0; i < array.length; i + size) {
+
+        // can't splice off more than remaining elements....
+        if (i + size > array.length) {
+            newArray.push(array.splice(i, array.length - i));
+        } else {
+            newArray.push(array.splice(i, size));
+        }
+    };
+
+    return (newArray);
+}
 
 module.exports = chunk;
