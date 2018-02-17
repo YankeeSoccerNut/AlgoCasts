92ec5d91ff85c31680a740bbf454473d1136474b
Scott Anderson <yankeesoccernut@gmail.com>
2018-02-16 21:45:58 -0500

    got solutions...minor tweaks

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,11 +7,23 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-    reverse = require('../reversestring/index');
+// function palindrome(str) {
+//     reverse = require('../reversestring/index');
 
-    let revStr = reverse(str);
-    return (str === revStr);
+//     let revStr = reverse(str);
+//     return (str === revStr);
+// }
+
+// another solution using the .every() on the array...
+// note this is NOT an optimal solution as it does 2x the work necessary
+// e.g. no need to check beyond the midpoint...we already checked
+
+function palindrome(str) {
+
+    return str.split("").every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+
 }
 
 module.exports = palindrome;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+
+//     let sign = 1;
+//     let digitArray = n.toString().split("");
+
+//     if (parseInt(n) < 0) {
+//         sign = -1;
+//         digitArray = digitArray.splice(1, digitArray.length - 1); 
+//     };
+
+//     return (parseInt(digitArray.reverse().join("")) * sign);
+// }
+
+// improved version....using Math.sign and taking advantage of 
+// of parseInt 'dropping' the sign after reversal
+
+function reverseInt(n) {
+
+    let digitArray = n.toString().split("");
+
+    return (parseInt(digitArray.reverse().join("")) * Math.sign(n));
+}
 
 module.exports = reverseInt;
