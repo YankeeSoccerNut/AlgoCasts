146ef27ed3a81f147c0ee5dba2f803edb08de211
Scott Anderson <yankeesoccernut@gmail.com>
2018-02-17 15:01:21 -0500

    use slice to eliminate side effects

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,22 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// my original solution used splice...not great because it creates side effects...
+// the original array is reduced to an empty array....
+// better solution is array.slice....it has no side effects.
+// function chunk(array, size) {
+
+//     let newArray = [];
+//     let numChunks = (array.length % size === 0) ? array.length / size : Math.round(array.length / size);
+
+//     for (let i = 0; i < array.length; i + size) {
+//         // can't splice off more than remaining elements....
+//         if (i + size > array.length) {
+//             newArray.push(array.splice(i, array.length - i));
+//         } else {
+//             newArray.push(array.splice(i, size));
+//         }
+//     };
+//     return (newArray);
+// }
+
+// much improved solution....use slice to eliminate side effects and take
+// advantage of the fact that slice won't go beyond array.length!
 function chunk(array, size) {
 
     let newArray = [];
-    let numChunks = (array.length % size === 0) ? array.length / size : Math.round(array.length / size);
 
-    for (let i = 0; i < array.length; i + size) {
-
-        // can't splice off more than remaining elements....
-        if (i + size > array.length) {
-            newArray.push(array.splice(i, array.length - i));
-        } else {
-            newArray.push(array.splice(i, size));
-        }
+    for (let i = 0; i < array.length; i += size) {
+        newArray.push(array.slice(i, i + size));
     };
-
     return (newArray);
-}
+};
 
 module.exports = chunk;
